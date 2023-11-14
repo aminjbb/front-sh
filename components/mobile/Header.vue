a87a002b0b47c10e797310b16668c78ee597f7db
zohreh <zohreh.tt@gmail.com>
2023-11-14 14:54:43 +0330

    feat(mobile menu): Add mobile menu

--- components/mobile/Header.vue
+++ components/mobile/Header.vue
@@ -24,6 +24,7 @@
         </div>
     </div>
 </header>
+<mobileMenu />
 </template>
 
 <script>
@@ -54,13 +55,18 @@ export default {
             if (window.scrollY > 60) {
                 this.isHidden = true;
                 this.isFixed = false;
+                const menu = document.getElementById('menu--mobile');
 
                 if (currentScrollTop > this.lastScrollTop) {
                     this.isHidden = true;
                     this.isFixed = false;
+
+                    menu.classList.add('change-height');
                 } else {
                     this.isFixed = true;
                     this.isHidden = false;
+
+                    menu.classList.remove('change-height');
                 }
 
                 this.lastScrollTop = currentScrollTop;
@@ -71,61 +77,5 @@ export default {
 </script>
 
 <style lang="scss">
-$parent:'header';
-
-.#{$parent} {
-    &--mobile {
-        position: fixed;
-        top: 0;
-        right: 0;
-        overflow: hidden;
-        transition: opacity 0.3s ease-in-out;
-        opacity: 0;
-        width: 100%;
-        height: 56px;
-        background: #fff;
-        box-shadow: 0px 6px 6px -3px rgba(126, 126, 126, 0.10);
-        padding: 8px;
-
-        .#{$parent} {
-            &__logo {
-                margin-left: 16px;
-            }
-
-            &__search-box {
-                flex: 1;
-                height: 40px;
-
-                .v-input__control,
-                .v-field__prepend-inner,
-                .v-field__field {
-                    height: 40px;
-                }
-
-                .v-field {
-                    box-shadow: none;
-                    background: #EEEEEE;
-                    border-radius: 2px;
-                }
-
-                .v-field__input,
-                .v-label {
-                    font-size: 13px !important;
-                }
-            }
-        }
-
-        &.hidden {
-            opacity: 0;
-            transform: translateY(-100%);
-            transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
-        }
-
-        &.fixed {
-            opacity: 1;
-            transform: translateY(0);
-            transition: opacity 0.3s ease-in-out, transform 0.3s ease-in-out;
-        }
-    }
-}
+@import '~/assets/scss/components/mobile/general/header.scss';
 </style>
