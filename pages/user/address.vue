75bc31ba659c03dc986141981ff51b89b0dcbb18
zohreh <zohreh.tt@gmail.com>
2023-12-18 16:19:31 +0330

    feat(user): Add user address and favorite page

--- pages/user/address.vue
+++ pages/user/address.vue
@@ -8,7 +8,7 @@
             <div class="col-9 pa-4">
                 <v-card class="pa-5 mobile-pa-0 mobile-no-border">
                     <header class="d-flex align-center justify-space-between mb-5">
-                        <span>لیست علاقمندی‌ها</span>
+                        <span>آدرس‌های شما</span>
                         
                         <generalUserAddressModal title="ثبت آدرس جدید" buttonType="text"/>
                     </header>
