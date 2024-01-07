794bba6944ab564ba8dada3b49ede91b3d5b66b5
zohreh <zohreh.tt@gmail.com>
2024-01-07 12:04:45 +0330

    feat(FAQ): Add faq page

---
+++ components/general/Breadcrumb.vue
@@ -0,0 +1,21 @@
+<template lang="">
+<div v-if="items && items.length">
+    <v-breadcrumbs :items="items">
+        <template v-slot:divider>
+            <span>/</span>
+        </template>
+    </v-breadcrumbs>
+</div>
+</template>
+
+<script>
+export default {
+    props:{
+        items:Array
+    }
+}
+</script>
+
+<style>
+@import '~/assets/scss/components/general/breadcrumbs.scss';
+</style>
