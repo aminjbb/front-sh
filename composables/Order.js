97449d4245c0316c379c7a72b0c8f824db3a8b8f
amin jabari <aminjabari.dev@gmail.com>
2024-01-19 14:15:05 +0330

    fix(order-list):fix end point

--- composables/Order.js
+++ composables/Order.js
@@ -3,17 +3,22 @@
  */
 import { ref} from 'vue';
 import { AxiosCall } from '~/utils/axios_call.js'
-
+import axios from 'axios'
 export default function setup(posts) {
     const orderList = ref([]);
     const loading = ref(false)
     const runtimeConfig = useRuntimeConfig()
-
+    const userToken = useCookie('userToken')
+    /** get user order list **/
     async function getOrderList(query) {
         axios
-            .get(runtimeConfig.public.apiBase +`page/get-with-slug/${route.params.slug}`)
+            .get(runtimeConfig.public.apiBase +`/order/crud/index/` ,{
+                headers: {
+                    Authorization: `Bearer ${userToken.value}`,
+                },
+            })
             .then((response) => {
-                orderList.value = response.data.data
+                orderList.value = response
             })
             .catch((err) => {
 
