717ab4d644f7456a612e68f44d754b9db03fe7c2
amin jabari <aminjabari.dev@gmail.com>
2024-02-14 11:17:32 +0330

    feat(bascket):fix create order

--- components/mobile/cart/ProductCard.vue
+++ components/mobile/cart/ProductCard.vue
@@ -10,7 +10,7 @@
          class="t12 w400 l-28">
 
         <template v-if="content.site_price !== content.current_site_price ">
-          کالا افزایش قیمت داشته است.
+          کالا  تغییر قیمت داشته است.
         </template>
         <template v-else-if="content.site_stock === 0">
           کالا به اتمام رسیده است.
