52c292e0a0454d0a8f26d830f2b81f13c59936b6
Shavaz <shavaz@Shavazs-MacBook-Air.local>
2024-03-11 12:44:11 +0330

    feat(color slider): Add colors slider

--- components/general/Product/Single/Comments.vue
+++ components/general/Product/Single/Comments.vue
@@ -55,7 +55,7 @@
 export default {
     data() {
         return {
-            sliceLength: 2,
+            sliceLength: 3,
         }
     },
 
@@ -99,7 +99,7 @@ export default {
         showAllComment() {
             document.getElementById('show-more-comments').classList.add('hide');
             const commentsLength =  this.comments?.length;
-            this.sliceLength = sliceLength - 1
+            this.sliceLength = commentsLength - 1
         }
     },
 
