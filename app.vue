2ca66ade59b48e052f05ee1e45c76c8e3e758735
Shavaz <shavaz@Shavazs-MacBook-Air.local>
2024-03-31 18:11:55 +0330

    feat(seo codes): Add enhance ecommerce methods

--- app.vue
+++ app.vue
@@ -6,15 +6,15 @@
   </div>
 </template>
 
-<script setup>
+ <script setup>
 useHead({
-  noscript: [{ children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5RRH4PX"
+  noscript: [{ children: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXGSV42K"
 height="0" width="0" style="display:none;visibility:hidden"></iframe>
 ` }],
   script: [{ children: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5RRH4PX');` }],
+})(window,document,'script','dataLayer','GTM-TXGSV42K');` }],
 });
 </script>
