fe58a5e9ee85ce89514f3815c6dee80876058a0e
yosra feili <feiliyosra@gmail.com>
2024-05-08 13:33:38 +0330

    feat:(add skeleton brand and search)

--- pages/brand/[slug].vue
+++ pages/brand/[slug].vue
@@ -1,8 +1,123 @@
 <template>
   <main class="v-product v-product--list">
     <h1 class="v-hide">{{ title }}</h1>
+    <v-container v-show="loading">
+      <v-row v-if="screenType === 'desktop'">
+        <v-col cols="1">
+          <v-skeleton-loader
+              :loading="loading"
+              type="text"/>
+        </v-col>
+      </v-row>
 
-    <v-container>
+      <v-row class="mt-10">
+        <v-col
+            cols="12"
+            md="3"
+            v-if="screenType === 'desktop'">
+          <div class="filter-sidebar">
+            <v-row justify="space-between">
+              <v-col
+                  class="mb-2"
+                  cols="6">
+                <v-skeleton-loader
+                    :loading="loading"
+                    type="text">
+                </v-skeleton-loader>
+              </v-col>
+              <v-col
+                  class="mb-2"
+                  cols="6">
+                <v-skeleton-loader
+                    :loading="loading"
+                    type="text">
+                </v-skeleton-loader>
+              </v-col>
+            </v-row>
+            <v-row justify="start">
+              <v-col
+                  v-for="sidBarItem in 3"
+                  :key="sidBarItem"
+                  cols="12">
+                <v-skeleton-loader
+                    :loading="loading"
+                    type="text">
+                </v-skeleton-loader>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+
+        <v-row justify="space-between" v-if="screenType === 'mobile'">
+          <v-col cols="6">
+            <v-skeleton-loader
+                v-if="loading"
+                type="text"/>
+          </v-col>
+          <v-col cols="6">
+            <v-skeleton-loader
+                v-if="loading"
+                type="text"/>
+          </v-col>
+        </v-row>
+
+
+        <v-col cols="12" md="9">
+          <template v-if="screenType === 'desktop'">
+            <v-row justify="start">
+              <v-col cols="1" v-for="cat in 5" :key="cat">
+                <v-skeleton-loader
+                    v-if="loading"
+                    type="text"/>
+              </v-col>
+            </v-row>
+          </template>
+
+          <div class="v-product__contents" :class="screenType === 'desktop' ? 'mt-6' : ''">
+            <div class="px-2">
+              <v-row>
+                <v-col
+                    v-for="card in 12"
+                    :key="card"
+                    cols="12"
+                    lg="3">
+                  <v-skeleton-loader
+                      :loading="loading"
+                      height="240"
+                      type="image, list-item-two-line"
+                  >
+                    <v-responsive>
+                      <v-img
+                          src="src"
+                          class="rounded-lg mb-2"
+                          height="184"
+                          cover/>
+                      <v-list-item
+                          subtitle="subtitle"
+                          title="title"
+                          class="px-0"/>
+                    </v-responsive>
+                  </v-skeleton-loader>
+                </v-col>
+              </v-row>
+            </div>
+          </div>
+
+          <div class="v-product__pagination d-flex justify-center mt-8">
+            <v-row justify="center">
+              <v-col v-for="num in 6" :key="num" cols="1">
+                <v-skeleton-loader
+                    class="mx-auto"
+                    max-width="100"
+                    type="button"/>
+              </v-col>
+            </v-row>
+          </div>
+        </v-col>
+      </v-row>
+    </v-container>
+
+    <v-container  v-show="!loading">
       <generalBreadcrumb :items="breadcrumbList"/>
       <v-row class="mt-10">
         <v-col cols="12" md="3">
@@ -105,7 +220,8 @@ export default {
       breadcrumb,
       query,
       plpTitle,
-      description
+      description,
+      loading
     } = new PLP()
     useHead({
       title,
@@ -124,7 +240,8 @@ export default {
       breadcrumb,
       query,
       plpTitle,
-      title
+      title,
+      loading
     }
   },
 
--- pages/search/index.vue
+++ pages/search/index.vue
@@ -1,61 +1,115 @@
 <template>
 <main class="v-product v-product--list">
-    <h1 class="v-hide">{{ title }}</h1>
+  <h1 class="v-hide">{{ title }}</h1>
 
-    <v-container>
-        <v-row class="mt-10">
-            <v-col cols="12">
-                <template v-if="screenType === 'desktop'">
-                    <div class="v-product__filter d-flex pt-1 align-center justify-space-between">
-                        <nav class="d-flex align-center flex-grow-1">
-                            <div class="pl-4">
-                                <v-icon icon="mdi-sort-ascending" color="grey-darken-1" />
-                                <span class="t14 w400 text-grey-darken-1">مرتب‌سازی بر اساس:</span>
-                            </div>
+  <v-container v-show="loading" >
+    <v-row justify="start">
+      <v-col cols="4">
+        <v-skeleton-loader
+            :loading="loading"
+            type="text"/>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col cols="12">
+        <div class="v-product__contents">
+          <div class="px-2">
+            <v-row>
+              <v-col
+                  v-for="card in 12"
+                  :key="card"
+                  cols="12"
+                  lg="3">
+                <v-skeleton-loader
+                    :loading="loading"
+                    height="240"
+                    type="image, list-item-two-line"
+                >
+                  <v-responsive>
+                    <v-img
+                        src="src"
+                        class="rounded-lg mb-2"
+                        height="184"
+                        cover/>
+                    <v-list-item
+                        subtitle="subtitle"
+                        title="title"
+                        class="px-0"/>
+                  </v-responsive>
+                </v-skeleton-loader>
+              </v-col>
+            </v-row>
+          </div>
+        </div>
 
-                            <ul class="v-product__filter__items d-flex align-center">
-                                <li class="t14 w400 px-4" :class="(sortType=== 'created_at' && orderType === 'desc') ? 'text-primary' : 'text-grey' " @click="sort('created_at', 'desc')">جدیدترین</li>
-                                <li class="t14 w400 px-4" :class="(sortType=== 'site_price' && orderType === 'asc') ? 'text-primary' : 'text-grey' " @click="sort('site_price', 'asc')">ارزان‌ترین</li>
-                                <li class="t14 w400 px-4" :class="(sortType=== 'site_price' && orderType === 'desc') ? 'text-primary' : 'text-grey' " @click="sort('site_price', 'desc')">گران‌ترین</li>
-                                <li class="t14 w400 px-4" :class="(sortType=== 'discount' && orderType ===  'desc') ? 'text-primary' : 'text-grey' " @click="sort('discount', 'desc')">بیشترین تخفیف</li>
-                            </ul>
-                        </nav>
-                    </div>
-                </template>
-                <div class="v-product__contents" :class="screenType === 'desktop' ? 'mt-6' : ''">
-                    <v-row v-if="productListData?.length" class="ma-0">
-                        <v-col
-                            cols="12"
-                            md="3"
-                            v-for="(item, index) in productListData"
-                            :key="`card-${index}`"
-                            class="v-product__content d-flex">
-                            <generalProductCard
-                                :content="item"
-                                :lazy=false
-                                class="mb-4 flex-grow-1"
-                                :hideInfo="true"
-                                :isPLP="true"
-                                :index = "index + 1"
-                                :sectionName = "`${query}لیست کالاهای`"
-                                :showColors="true" />
-                        </v-col>
-                    </v-row>
-                </div>
-
-                <div class="v-product__pagination d-flex justify-center mt-8">
-                    <v-pagination
-                        v-model="page"
-                        :length="productListPageLength"
-                        size="40"
-                        :total-visible="6"
-                        @click="backToTop"
-                        prev-icon="mdi-chevron-right"
-                        next-icon="mdi-chevron-left" />
-                </div>
+        <div class="v-product__pagination d-flex justify-center mt-8">
+          <v-row justify="center">
+            <v-col v-for="num in 6" :key="num" cols="1">
+              <v-skeleton-loader
+                  class="mx-auto"
+                  max-width="100"
+                  type="button"/>
             </v-col>
-        </v-row>
-    </v-container>
+          </v-row>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+
+  <v-container v-show="!loading">
+    <v-row class="mt-10">
+      <v-col cols="12">
+        <template v-if="screenType === 'desktop'">
+          <div class="v-product__filter d-flex pt-1 align-center justify-space-between">
+            <nav class="d-flex align-center flex-grow-1">
+              <div class="pl-4">
+                <v-icon icon="mdi-sort-ascending" color="grey-darken-1" />
+                <span class="t14 w400 text-grey-darken-1">مرتب‌سازی بر اساس:</span>
+              </div>
+
+              <ul class="v-product__filter__items d-flex align-center">
+                <li class="t14 w400 px-4" :class="(sortType=== 'created_at' && orderType === 'desc') ? 'text-primary' : 'text-grey' " @click="sort('created_at', 'desc')">جدیدترین</li>
+                <li class="t14 w400 px-4" :class="(sortType=== 'site_price' && orderType === 'asc') ? 'text-primary' : 'text-grey' " @click="sort('site_price', 'asc')">ارزان‌ترین</li>
+                <li class="t14 w400 px-4" :class="(sortType=== 'site_price' && orderType === 'desc') ? 'text-primary' : 'text-grey' " @click="sort('site_price', 'desc')">گران‌ترین</li>
+                <li class="t14 w400 px-4" :class="(sortType=== 'discount' && orderType ===  'desc') ? 'text-primary' : 'text-grey' " @click="sort('discount', 'desc')">بیشترین تخفیف</li>
+              </ul>
+            </nav>
+          </div>
+        </template>
+        <div class="v-product__contents" :class="screenType === 'desktop' ? 'mt-6' : ''">
+          <v-row v-if="productListData?.length" class="ma-0">
+            <v-col
+                cols="12"
+                md="3"
+                v-for="(item, index) in productListData"
+                :key="`card-${index}`"
+                class="v-product__content d-flex">
+              <generalProductCard
+                  :content="item"
+                  :lazy=false
+                  class="mb-4 flex-grow-1"
+                  :hideInfo="true"
+                  :isPLP="true"
+                  :index = "index + 1"
+                  :sectionName = "`${query}لیست کالاهای`"
+                  :showColors="true" />
+            </v-col>
+          </v-row>
+        </div>
+
+        <div class="v-product__pagination d-flex justify-center mt-8">
+          <v-pagination
+              v-model="page"
+              :length="productListPageLength"
+              size="40"
+              :total-visible="6"
+              @click="backToTop"
+              prev-icon="mdi-chevron-right"
+              next-icon="mdi-chevron-left" />
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
 </main>
 </template>
 
@@ -80,7 +134,8 @@ export default {
             productList,
             filterQuery,
             page,
-            query
+            query,
+            loading
         } = new PLP()
 
         useHead({
@@ -95,7 +150,8 @@ export default {
             productList,
             filterQuery,
             page,
-            query
+            query,
+            loading
         }
     },
 
