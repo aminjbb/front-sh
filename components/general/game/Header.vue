d44b00bc4725e280a8b1502b1f57f63ef2bbccf9
shayan bazargan <shayanbazargun@gmail.com>
2024-06-23 17:12:46 +0330

    fix(luckywheel): luckywheel header ui/ux improvement

--- components/general/game/Header.vue
+++ components/general/game/Header.vue
@@ -1,8 +1,14 @@
 <template>
-    <header class="game-header">
-        <a href="/" title="Shavaz logo">
-            <img data-not-lazy src="~/assets/images/mobile-logo.svg" class="" alt="Shavaz Logo" width="107" height="36" title="Shavaz Logo" />
-        </a>
+    <header >
+        <div class="text-center align-center mx-5 py-3">
+            <a href="/" title="Shavaz logo" >
+                <img data-not-lazy src="~/assets/images/mobile-logo.svg"  alt="Shavaz Logo" width="107" height="36" title="Shavaz Logo" />
+            </a>
+            <a href="/">
+                <v-icon color="white" class="float-left " >mdi-logout</v-icon>
+            </a>
+        </div>
+
     </header>
 </template>
 
@@ -12,17 +18,3 @@ export default {
 }
 </script>
 
-<style lang="scss" scoped>
-    .game-header{
-        height: 64px;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        padding-top:5px ;
-
-        a {
-            height: 36px;
-            display: inline-block;
-        }
-    }
-</style>
