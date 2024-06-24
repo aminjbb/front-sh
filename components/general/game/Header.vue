b7b76abaf4329738930fa4ebc1d66adf8e870c68
shayan bazargan <shayanbazargun@gmail.com>
2024-06-24 12:21:46 +0330

    fix(luckyWeel): 'logout btn added'

--- components/general/game/Header.vue
+++ components/general/game/Header.vue
@@ -5,7 +5,7 @@
                 <img data-not-lazy src="~/assets/images/mobile-logo.svg"  alt="Shavaz Logo" width="107" height="36" title="Shavaz Logo" />
             </a>
             <a href="/">
-                <v-icon color="white" class="float-left " >mdi-logout</v-icon>
+                <v-icon color="white" class="float-left " @click="logout">mdi-logout</v-icon>
             </a>
         </div>
 
@@ -14,6 +14,19 @@
 
 <script>
 export default {
+    setup() {
+        const userToken = useCookie('userToken')
+        return {
+            userToken,
+        }
+    },
+    methods: {
+        logout() {
+            this.userToken = '';
+             window.location = '/';
+        },
+
+    }
 
 }
 </script>
