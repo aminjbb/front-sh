c8368db283cc4df408359ed70c042272c23a9c90
amin jabari <aminjabari.dev@gmail.com>
2024-07-16 19:10:44 +0330

    fix(sitemap):add runtimeConfig in sitemap modules

--- modules/sitemapModule.js
+++ modules/sitemapModule.js
@@ -4,15 +4,15 @@ import { generateSitemaps } from '../utils/sitemapGenerator';
 export default defineNuxtModule({
     async setup(_, nuxt) {
       nuxt.hook('build:before', async () => {
-        const apiConfigs = [
-            { apiUrl: 'https://api.shvz.ir/api/v1/sitemap', fileName: 'sitemap.xml' },
-            { apiUrl: 'https://api.shvz.ir/api/v1/sitemap/main', fileName: 'sitemap-main.xml' },
-            { apiUrl: 'https://api.shvz.ir/api/v1/sitemap/products?page_number=1', fileName: 'products-sitemap-1.xml' },
-            { apiUrl: 'https://api.shvz.ir/api/v1/sitemap/categories?page_number=1', fileName: 'categories-sitemap-1.xml' },
-            { apiUrl: 'https://api.shvz.ir/api/v1/sitemap/brands?page_number=1', fileName: 'brands-sitemap-1.xml' },
-            { apiUrl: 'https://api.shvz.ir/api/v1/sitemap/skus?page_number=1', fileName: 'sku-sitemap-1.xml' },
-            { apiUrl: 'https://api.shvz.ir/api/v1/sitemap/skus?page_number=2', fileName: 'sku-sitemap-2.xml' },
-            { apiUrl: 'https://api.shvz.ir/api/v1/sitemap/game-gardoone?page_number=1', fileName: 'game-gardoone-sitemap-1.xml' },
+          const apiConfigs = [
+            { apiUrl: `${nuxt.options.runtimeConfig.public.apiBase}/sitemap` , fileName: 'sitemap.xml' },
+            { apiUrl: `${nuxt.options.runtimeConfig.public.apiBase}/sitemap/main`, fileName: 'sitemap-main.xml' },
+            { apiUrl: `${nuxt.options.runtimeConfig.public.apiBase}/sitemap/products?page_number=1`, fileName: 'products-sitemap-1.xml' },
+            { apiUrl: `${nuxt.options.runtimeConfig.public.apiBase}/sitemap/categories?page_number=1`, fileName: 'categories-sitemap-1.xml' },
+            { apiUrl: `${nuxt.options.runtimeConfig.public.apiBase}/sitemap/brands?page_number=1`, fileName: 'brands-sitemap-1.xml' },
+            { apiUrl: `${nuxt.options.runtimeConfig.public.apiBase}/sitemap/skus?page_number=1`, fileName: 'sku-sitemap-1.xml' },
+            { apiUrl: `${nuxt.options.runtimeConfig.public.apiBase}/sitemap/skus?page_number=2`, fileName: 'sku-sitemap-2.xml' },
+            { apiUrl: `${nuxt.options.runtimeConfig.public.apiBase}/sitemap/game-gardoone?page_number=1`, fileName: 'game-gardoone-sitemap-1.xml' },
         ];
         await generateSitemaps(apiConfigs);
       });
