aa98b36d9e40ea105be3a5738e0679746ab455f0
Shavaz <shavaz@Shavazs-MacBook-Air.local>
2024-09-18 15:33:31 +0330

    feat(ticket single page): redesign techet details and cards

--- pages/user/ticket/[id].vue
+++ pages/user/ticket/[id].vue
@@ -4,7 +4,7 @@
         <a href="/user/ticket" class="ml-3">
             <v-icon icon="mdi-arrow-right" color="grey-darken-3" />
         </a>
-        <span class="grey-darken-3 t14">جزئیات تیکت</span>
+        <span class="text-sGrayDarken2 t14 w700">جزئیات تیکت</span>
     </header>
 
     <v-container>
@@ -14,46 +14,53 @@
             </div>
 
             <div class="col-9 pa-4 mobile-pa-0">
-                <v-card class="pa-8 mobile-pa-0 mobile-no-border has-header">
-                    <header class="d-flex align-center justify-space-between mb-5 xs-hide card__header">
-                        <span>جزئیات تیکت</span>
+                <v-card class="pa-8 mobile-pa-0 v-ticket__card">
+                    <div class="flex-grow-1 d-flex flex-column" :class="isMobile === true ? 'mb-2' : 'pa-8'">
+                        <header class="v-ticket__header d-flex align-center justify-space-between mb-3 xs-hide">
+                            <h1 class="t18 w700 text-sGrayDarken2">{{ singleTicket?.parent_topic }}</h1>
 
-                        <v-btn
-                            v-if="singleTicket?.status !== 'resolved'"
-                            @click="showAnswer()"
-                            height="36"
-                            title="ثبت پاسخ"
-                            class="btn--cancel">
-                            ثبت پاسخ
-                        </v-btn>
-                    </header>
+                            <v-btn
+                                v-if="singleTicket?.status !== 'resolved'"
+                                href="/user/ticket"
+                                height="45"
+                                title="بازگشت"
+                                class="btn--cancel br12">
+                                بازگشت
+                            </v-btn>
+                        </header>
 
-                    <div class="single-ticket flex-grow-1 d-flex flex-column" :class="isMobile === true ? 'px-3 mb-2' : 'px-6 mb-8'">
-                        <generalTicketSingleInfoCard v-if="singleTicket" :content="singleTicket" />
+                        <generalTicketSingleInfoCard v-if="singleTicket" :content="singleTicket" :more="singleTicket.status !== 'open' ? true : false"/>
 
-                        <div v-if="singleTicket" class="single-ticket__list">
-                            <generalTicketUserAnswer :content="singleTicket.content" :fileList="singleTicket.files"/>
+                        <div v-if="singleTicket?.threads" class="v-ticket__list mt-6">
+                            
+                            <div class="v-ticket__list__sort" v-for="(ticketListByDate, index) in singleTicket.threads" :key="`parent${index}`">
 
-                            <template v-if="singleTicket.threads">
-                                <template v-for="ticket in singleTicket.threads" :key="ticket.id">
-                                    <template v-if="ticket.creator === 'user'">
-                                        <generalTicketUserAnswer :content="ticket.content" :fileList="ticket.files"/>
-                                    </template>
-                                    <template v-if="ticket.creator === 'admin'">
-                                        <generalTicketAdminAnswer :content="ticket.content" :fileList="ticket.files"/>
-                                    </template>
+                                <header v-if="ticketListByDate.date" class="d-flex align-center justify-center">
+                                    <span class="bg-white px-2">
+                                        <span class="px-4 py-2 t12 w400 text-primary number-font s-border br12">{{ticketListByDate.date}}</span>
+                                    </span>
+                                </header>
+
+                                <template v-if="ticketListByDate.items">
+                                    <generalTicketAnswerCard v-for="(ticket, index) in ticketListByDate.items" :key="`child${index}`" :content="ticket" class="mb-6" :status="ticket?.creator === 'admin' ? 'admin' : 'user'" :class="ticket?.creator === 'admin' ? 'pb-9' : ''"/>
                                 </template>
-                            </template>
+                            </div>
                         </div>
                     </div>
                 </v-card>
 
-                <v-divider
-                    v-if="isMobile"
-                    color="grey-lighten-1"
-                    class="mb-3" />
+                <div v-if="isMobile" class="xs-show px-3">
+                    <v-btn
+                        href="/user/ticket"
+                        height="45"
+                        title="بستن جزئیات"
+                        block
+                        class="btn--cancel w-100 br12 no-shadow">
+                        بستن جزئیات
+                    </v-btn>
+                </div>
 
-                <div v-if="!showAnswerBox && singleTicket?.status !== 'resolved'" class="xs-show px-3">
+                <!-- <div v-if="!showAnswerBox && singleTicket?.status !== 'resolved'" class="xs-show px-3">
                     <v-btn
                         @click="showAnswer()"
                         height="36"
@@ -61,9 +68,9 @@
                         class="btn--cancel w-100">
                         ثبت پاسخ
                     </v-btn>
-                </div>
+                </div> -->
 
-                <v-card v-if="showAnswerBox && singleTicket?.status !== 'resolved'" class="pa-8 mobile-pa-0 mobile-no-border">
+                <!-- <v-card v-if="showAnswerBox && singleTicket?.status !== 'resolved'" class="pa-8 mobile-pa-0 mobile-no-border">
                     <div class="ticket__form px-3">
                         <v-form ref="ticket" v-model="valid">
                             <v-row>
@@ -107,7 +114,7 @@
                             </v-btn>
                         </div>
                     </div>
-                </v-card>
+                </v-card> -->
             </div>
         </v-row>
     </v-container>
