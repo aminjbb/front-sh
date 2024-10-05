fc6b6f37fd49d560a276cdfaa70ab20a19050903
yosra <feiliyosra@gmail.com>
2024-10-05 12:26:32 +0330

    feat(image):image component

---
+++ components/general/Kits/Image/s-image.vue
@@ -0,0 +1,24 @@
+<template>
+  <img
+      :src="imgSrc"
+      :title="imgTitle"
+      :alt="imgAlt"
+      :width="width"
+      :height="height" />
+</template>
+
+<script>
+export default {
+  props:{
+    imgSrc: String,
+    imgAlt: String,
+    imgTitle: String,
+    width: Number,
+    height: Number,
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "~/components/general/Kits/Image/s-image.scss";
+</style>
